414db6d2f2faf6110fb8bade9169280e3c4db438
prateek00077 <pandeyprateek560@gmail.com>
2025-06-10 13:47:09 +0530

    added vercel.json for production

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="node" />
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,7 +9,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': process.env.VITE_BACKEND_PUBLIC_URI ?? 'http://localhost:3000', // change to your backend port if different
+      '/api':'http://localhost:3000', // change to your backend port if different
     },
   },
 });
